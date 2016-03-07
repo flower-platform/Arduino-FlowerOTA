10dc12d2f6be1c68f3c4df82945159188be316b5
Claudiu Matei <claudiu@shmessenger.org>
2016-03-07 16:29:59 +0200

    Initial commit

---
+++ examples/EthernetOTA/EthernetOTA.ino
@@ -0,0 +1,61 @@
+/* license-start
+ * 
+ * Copyright (C) 2016 Crispico Resonate, <http://www.crispico.com/>.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details, at <http://www.gnu.org/licenses/>.
+ * 
+ * license-end
+ */
+
+#include <FlowerOTA.h>
+#include <EthernetUDP.h>
+
+FlowerOTA ota;
+EthernetUDP udp;
+EthernetClient client;
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 100, 251);
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(13, OUTPUT);
+  pinMode(13, HIGH);
+  
+  pinMode(SS, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(4, HIGH);
+  
+
+  Ethernet.begin(mac, ip);
+  ota.begin(&udp, &client);
+  
+
+}
+
+long deadline;
+uint8_t led;
+
+void loop() {
+  ota.loop();
+  
+  if (millis() > deadline) {
+    Serial.println("loop");
+    if (led == HIGH) {
+      led = LOW;
+    } else {
+      led = HIGH;
+    }
+    digitalWrite(13, led);
+    deadline = millis() + 500;
+  }
+}
+
+
---
+++ examples/WiFiOTA/WiFiOTA.ino
@@ -0,0 +1,69 @@
+/* license-start
+ * 
+ * Copyright (C) 2016 Crispico Resonate, <http://www.crispico.com/>.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details, at <http://www.gnu.org/licenses/>.
+ * 
+ * license-end
+ */
+ 
+#include <FlowerOTA.h>
+#include <SPI.h>
+#include <WiFi101.h>
+#include <WiFiUDP.h>
+
+FlowerOTA ota;
+WiFiUDP udp;
+WiFiClient client;
+IPAddress ip(192, 168, 100, 251);
+
+void setup() {
+  Serial.begin(115200);
+//  while (!Serial);
+
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
+  
+  char* ssid = "ssid";     
+  char* pass = "password";  
+  
+  WiFi.config(ip);
+  int status = WL_IDLE_STATUS;
+  while ( status != WL_CONNECTED) {
+    Serial.print("Conntecting to: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(3000);
+  }
+  Serial.println("Connected");
+
+  ota.begin(&udp, &client);
+
+}
+
+long deadline = 0;
+
+uint8_t led;
+
+void loop() {
+  ota.loop();
+  
+  if (millis() > deadline) {
+    Serial.println("loop");
+    if (led == HIGH) {
+      led = LOW;
+    } else {
+      led = HIGH;
+    }
+    digitalWrite(6, led);
+    deadline = millis() + 500;
+  }
+}
+
